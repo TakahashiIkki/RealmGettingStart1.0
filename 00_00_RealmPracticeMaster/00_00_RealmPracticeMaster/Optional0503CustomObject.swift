d0e151a3921cdb39634ec413defda107e5c7ed09
TakahashiIkki <new_ittyandesu@outlook.jp>
2017-03-18 16:52:30 +0900

    Optional Bool

---
+++ 00_00_RealmPracticeMaster/00_00_RealmPracticeMaster/Optional0503CustomObject.swift
@@ -0,0 +1,9 @@
+//
+//  Optional0503CustomObject.swift
+//  00_00_RealmPracticeMaster
+//
+//  Created by 一騎高橋 on 2017/03/18.
+//  Copyright © 2017年 IkkiTakahashi. All rights reserved.
+//
+
+import Foundation
